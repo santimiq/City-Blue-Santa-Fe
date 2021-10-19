c4928b475048d892ea0f1c585418dcbf8fbbb81c
Santiago Miqueo <smiqueo@hotmail.com>
2021-10-19 12:37:55 -0500

    Remove logo in terms

--- src/sections/TermsCondition/TermsConditionSection.js
+++ src/sections/TermsCondition/TermsConditionSection.js
@@ -1,6 +1,5 @@
 import React from "react"
 import { Col, Container, Row } from "react-bootstrap"
-import Logo from "~image/logo/cb-logo-sup-blue.png"
 
 import Terms from "./style"
 export default function TermsCondition() {
@@ -8,13 +7,10 @@ export default function TermsCondition() {
     <Terms>
       <Container>
         <Row className="justify-content-center">
-          <Col className="col-xl-8 col-lg-8 col-md-6 col-xs-12 text-center">
+          <Col className="col-xl-12 col-lg-11 col-xs-11 text-center">
             <Terms.Box>
               <Terms.Title as="h2">Aviso de Privacidad</Terms.Title>
             </Terms.Box>
-          </Col>
-          <Col className="col-xl-4 col-lg-4 col-md-6 col-xs-12 text-center">
-            <img src={Logo} />
           </Col>
         </Row>
         <Row className="justify-content-center">
